d1294e8e01205a18ee6edd513c7f9e47b33a6713
prstmalepati <balaji.k.malepati@gmail.com>
2026-02-15 17:23:28 +0100

    Add files via upload

---
+++ src/pages/Calculators.tsx
@@ -0,0 +1,8 @@
+export default function Calculators() {
+  return (
+    <div style={{ padding: "2rem" }}>
+      <h1>Calculators</h1>
+      <p>Wealth projection & financial tools coming here.</p>
+    </div>
+  );
+}
