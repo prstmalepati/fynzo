1f85348f284862ae26c6f5203bb39a36b6e789c0
prstmalepati <balaji.k.malepati@gmail.com>
2026-02-15 10:40:47 +0100

    Delete src directory
    Delete, will load new

--- src/utils/fire.js
+++
@@ -1,3 +0,0 @@
-export const fireNumber = expenses => expenses * 12 * 25;
-export const fireProgress = (nw, expenses) =>
-  Math.min(100, Math.round((nw / fireNumber(expenses)) * 100));
--- src/utils/taxDE.js
+++
@@ -1,9 +0,0 @@
-export function germanIncomeTax(gross, married=false) {
-  const base = married ? gross/2 : gross;
-  let tax = base <= 10908 ? 0 :
-            base <= 15999 ? (base-10908)*0.14 :
-            base <= 62809 ? 1027+(base-15999)*0.24 :
-            14753+(base-62809)*0.42;
-  tax *= married ? 2 : 1;
-  return Math.round(tax * 1.055);
-}
