5bb936d01d0b00b8015131586ea4aac38ddab532
prstmalepati <balaji.k.malepati@gmail.com>
2026-02-15 21:01:40 +0100

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,20 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+
+import Overview from "./pages/Overview";
+import ProjectionPage from "./pages/ProjectionPage";
+import Calculators from "./pages/Calculators";
+import Taxes from "./pages/Taxes";
+import Settings from "./pages/Settings";
+
 export default function App() {
   return (
-    <div style={{ padding: 40 }}>
-      <h1>fynzo is LIVE 🚀</h1>
-      <p>If you see this, React is mounted.</p>
-    </div>
+    <Routes>
+      <Route path="/" element={<Overview />} />
+      <Route path="/projection" element={<ProjectionPage />} />
+      <Route path="/calculators" element={<Calculators />} />
+      <Route path="/taxes" element={<Taxes />} />
+      <Route path="/settings" element={<Settings />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
   );
 }
